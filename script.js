62661bf8871a59915e37cb4692a61dcfb45e073b
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-06-13 08:15:20 -0500

    Update to JS Review Lab

--- script.js
+++ script.js
@@ -124,3 +124,72 @@ if(fansAge >= 16){
 }
 
 
+
+
+/**II. Loops
+Remember: USE let when you initialize your for loops!
+
+This is GOOD: for(let i = 0; i < 100; i++){code to be executed}
+  // this is the for loop synthax i.e
+   for (expresion1;expression2(defines condition for executing the code block/for loop to run); expresion3 (executed every time, after the code block has been executed by increasing a value)) {
+    code block to be executed i.e. console.log(i)
+  }
+
+This is NO GOOD: for(i = 0; i < 100; i++) **/
+
+// A. The basics
+// Write a loop that will print out all the numbers from 0 to 10, inclusive
+  Ans:for(let i = 0; i<=10,i++){
+     console.log(i)
+     }
+  
+// Write a loop that will print out all the numbers from 10 up to and including 400
+     Ans: for(let i = 10; i<=400; i++){
+           System.out.println(i)
+        }
+// Write a loop that will print out every third number starting with 12 and going no higher than 4000
+  Ans: //Use oddEven synthax and Modulo Operator
+      
+   let number = 12;
+   System.out.println('odd')
+   System.out.println('even')
+
+
+
+// B. Get even
+// Print out the numbers that are within the range of 1 - 100
+// Adjust your code to add a message next to even numbers only that says: "<-- is an even number"
+
+
+
+// C. Give me Five
+// For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five
+// Example Output:
+
+// I found a 5. High five!
+// I found a 10. High five!
+
+
+// Add to the code from above to print out "I found a number. Three is a crowd" if the number is a multiple of three
+// Example Output:
+
+// I found a 3. Three is a crowd
+// I found a 5. High five!
+// I found a 6. Three is a crowd
+// I found a 9. Three is a crowd
+// I found a 10. High five!
+
+
+// For numbers divisible by both three and five, be sure your code prints both messages
+
+
+
+// D. Savings account
+// Write code that will save the sum of all the numbers between 1 - 10 to a variable called bank_account.
+// Check your work! Your bank_account should have $55 in it.
+
+// You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
+// Check your work! Your bank_account should have $10,100 in it.
+
+
+
