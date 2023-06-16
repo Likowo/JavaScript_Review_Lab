b58eaf642108fbe077b4139564c8e3e80b2b4935
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-06-15 21:40:40 -0500

    Update to JS Review lab

--- script.js
+++ script.js
@@ -364,13 +364,105 @@ if(fingers < 5){
 // ];
 
 // 1.What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+//****console.log(`Kristyn is rocking that $(kristynsCloset[2]) today!` )
 //2. Kristyn just bought some sweet shades! Add "raybans"to her closet after "yellow knit hat".
+     //*** */ kristynsCloset.splice(6,0,"raybans")
 //3. Kristyn spilled coffee on her hat... modify this item to read "stained knit hat"instead of yellow.
+kristynsCloset(5) = "stained knit hat"
+console.log(kristynsCloset)
 //4. Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirtsarray.
+thomsCloset[0][0]
+console.log(shirt)
 // 5.In the same way, access one item from Thom's pants array.
+let pants = thomsCloset[1][0]
 // 6.Access one item from Thom's accessories array.
+let accessories = thomsCloset[2][2]
 // 7.Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+console.log(`Thom is looking fierce in a ${shirt},${pants}, and ${accessories}`)
 //8. Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+thomsCloset[1][2] = " Footie Pajamas"
+console.log(thomsCloset)
+
+
+
+
+// IV. Functions
+
+
+// A. printGreeting
+// Do you think you could write a function called printGreetingwith a parameter namethat returns a greeting with the argument interpolated into the greeting?
+// Like so?
+// console.log(printGreeting("Slimer"));
+// => Hello there, Slimer!
+
+// You think you could? I think so too. Feel free to skip this problem, because you've already done it. If you've done the problem twice, read entire problems carefully before doing them from now on.
+
+
+// B. printCool
+// Write a function printCoolthat accepts one parameter, nameas an argument. The function should print the name and a message saying that that person is cool.
+const printCool = (name) => {
+  console.log(`${name} is cool`)
+}
+printCool ("captain Reynolds")
+
+// console.log(printCool("Captain Reynolds"));
+// => "Captain Reynolds is cool";
+
+
+
+
+// C. calculateCube
+// Write a function calculateCubethat takes a single number and prints the volume of a cube made from that number.
+
+// console.log(calculateCube(5));
+// => 125
+
+
+
+
+// D. isVowel
+// Write a function isVowelthat takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise. The vowel could be upper or lower case. Test your function on every vowel and make sure it's working. In general, when you write functions, take a minute to test them with different values to make sure they behave the way you want.
+
+// console.log(isVowel("a"));
+// => true
+
+
+
+
+// E. getTwoLengths
+// Write a function getTwoLengthsthat accepts two parameters (strings). The function should return an array of numbers where each number is the length of the corresponding string.
+
+// console.log(getTwoLengths("Hank", "Hippopopalous"));
+// => [4, 13]
+
+
+
+
+// F. getMultipleLengths
+// Write a function getMultipleLengthsthat accepts a single parameter as an argument: an array of strings. The function should return an array of numbers where each number is the length of the corresponding string.
+
+// console.log(getMultipleLengths(["hello", "what", "is", "up", "dude"]));
+// => [5, 4, 2, 2, 4]
+
+
+
+
+// G. maxOfThree
+// Define a function maxOfThreethat takes three numbers as arguments and returns the largest of them. If all numbers are the same, it doesn't matter which one is returned. If the two largest numbers are the same, one of them should be returned. Be sure to test it with larger values in each of the three locations.
+
+// console.log(maxOfThree(6, 9, 1));
+// => 9
+
+// Did you use Google and find Math.max()? If so, great job! Very resourceful—keep looking stuff up! However, for this particular question, we need you to submit a solution that does not use Math.max().
+
+
+
+
+// H. printLongestWord
+// Write a function printLongestWordthat accepts a single argument, an array of strings. The method should return the longest word in the array. In case of a tie, the method should return the word that appears first in the array.
+
+// console.log(printLongestWord(["BoJack", "Princess", "Diane", "a", "Max", "Peanutbutter", "big", "Todd"]));
+// => "Peanutbutter"
 
 
 
