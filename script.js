b760e075c74fd0a4818263b651271bcfcc71fda7
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-06-17 18:23:09 -0500

    Update to JS Review Objects

--- script.js
+++ script.js
@@ -501,50 +501,78 @@ const maxOfThree = (num1,num2,num3) => {
 // A. Make a user object
 // Create an object called user.
 // Write in to the object the key-value pairs for name, email, age, and purchased. Set the value of purchased to an empty array []. Set the other values to whatever you would like.
-
-
+const user = {
+  name: "Fanny",
+  email:"fan1@gmail.com",
+  age:22,
+  purchased:[]
+}
+console.log(user)
 
 // B. Update the user
 // Our user has changed his or her email address. Without changing the original userobject, update the emailvalue to a new email address.
 // Our user has had a birthday! Without changing the original userobject, increment the agevalue using the postfix operator. Hint: age++
-
+user.email = "biggy@gmail.com"
+user.age++
+console.log (user)
 
 
 // C. Adding keys and values
 // You have decided to add your user's location to the data that you want to collect.
-
-// Without changing the original userobject, add a new key locationto the object, and give it a value or some-or-other location (a string).
-
-
+// Without changing the original userobject, add a new key location to the object, and give it a value or some-or-other location (a string).
+user.location = "Alabama"
+console.log(user.location)
 
 // D. Shopaholic!
-// Our user has purchased an item! They have purchased some "carbohydrates". Using .push(), add the string "carbohydrates" to the purchasedarray.
-// Our user has purchased an item! They have purchased some "peace of mind". Using .push(), add the string "peace of mind" to the purchasedarray.
-// Our user has purchased an item! They have purchased some "Merino jodhpurs". Using .push(), add the string "Merino jodhpurs" to the purchasedarray.
-// Console.log just the "Merino jodhpurs" from the purchasedarray.
+// Our user has purchased an item! They have purchased some "carbohydrates". Using .push(), add the string "carbohydrates" to the purchased array.
+// Our user has purchased an item! They have purchased some "peace of mind". Using .push(), add the string "peace of mind" to the purchased array.
+// Our user has purchased an item! They have purchased some "Merino jodhpurs". Using .push(), add the string "Merino jodhpurs" to the purchased array.
+// Console.log just the "Merino jodhpurs" from the purchased array.
+user.purchased.push("carbohydrate")
+user.purchased.push("peace of mind")
+user.purchased.push("Merino jodhpurs")
+console.log(user.purchased[user.purchased.length -  1])
+// console.log(user.purchased.push(["Merino jodhpurs"]))
 
 
-
-// E. Object-within-object
+// E. Object-within-object ( Nested objects)
 // Remember that you can add an object to an existing object in the same way that you can add any new property/value pair.
-
-// If we want to give our user a friendwith a nameand age, we could write:
-
+// If we want to give our user object above, a friend with a name and age, we could write:
 // user.friend = {
 //     name: "Grace Hopper",
 //     age: 85
 // }
-// When we console.log user, we would see the friendobject added to our user object.
+//  console.log(user)
+// When we console.log user, we would see the friend object added to our user object.
 
-// Write a friendobject into your userobject and give the friend a name, age, location, and purchased array (empty for now)
-// Console.log just the friend's name
-// Console.log just the friend's location
-// CHANGE the friend's age to 55
-// The friendhas purchased "The One Ring". Use .push()to add "The One Ring" to the friend's purchasedarray.
-// The friendhas purchased "A latte". Use .push()to add "A latte" to the friend's purchasedarray.
-// Console.log just "A latte" from the friend's purchasedarray.
+// 1. Write a friend object into your user object and give the friend a name, age, location, and purchased array (empty for now)
+user.friend = {
+  name: "Alicia",
+  age: 24,
+  location: "Texas",
+  purchased: []
+}
 
+// 2. Console.log just the friend's name
+console.log(user.friend.name)
 
+// 3. Console.log just the friend's location
+console.log(user.friend.location)
+
+// 4. CHANGE the friend's age to 55
+user.friend.age = 55
+console.log(user.friend.age)
+
+// 5. The friend has purchased "The One Ring". Use .push()to add "The One Ring" to the friend's purchased array.
+user.friend.purchased = ["The One Ring"]
+//console.log(user.friend.purchased)
+
+//6. The friend has purchased "A latte". Use .push()to add "A latte" to the friend's purchased array.
+user.friend.purchased = ["A latte"]
+//console.log(user.friend.purchased)
+
+//7. Console.log just "A latte" from the friend's purchasedarray.
+console.log(user.friend.purchased[user.friend.purchased.length -  1])
 
 // F. Loops
 // Write a for loop that iterates over the User's purchasedarray (NOT the friend's purchased array), and prints each element to the console.
